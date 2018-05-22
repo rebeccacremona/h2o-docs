3318fdf2316f7c69303fc8851ab4fdb10a3c34b9
Adam Ziegler <abziegler@gmail.com>
2018-05-22 16:41:23 -0400

    Initial changes to home layout

--- assets/src/javascripts/home.js
+++ assets/src/javascripts/home.js
@@ -1,13 +1,4 @@
 $(document).ready(function(){
-  // header + nav should appx. fill screen on home page
-  var h = $(window).height();
-  var w = $(window).width();
-  var r = 1.8 * w;
-  if (r > h){
-    $('body.home header').height(h);
-  } else {
-    $('body.home header').height(r);
-  }
   // roundup
   showHopper("https://lil.law.harvard.edu/roundup/api/item");
 });
